f9fc9e244be8d4c21d0dafe4cfd4febdbffad3b3
Dylan Brain <dylanbrain@googlemail.com>
2021-02-08 20:45:07 +0900

    added new book functionality

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-let myLibrary = ["harry potter", " lord of the rings", " 1984", ""]
+let myLibrary = []
 let libDisplay = document.getElementById("libDisplay");
 let formButton = document.getElementById("formButton");
 let closeButton = document.getElementById("close-btn");
 
+let bookTitle = document.getElementById("bookTitle");
+let submitButton = document.getElementById("submit");
+
+let newbook;
+let title;
+
+let displayArray = []
+
 function Book() {
+    this.title = bookTitle.value;
+    //this.read = 
     // constructor function
 }
 
 function addBookToLibrary () {
+    console.log("addBookToLibrary Launched")
+    newBook = new Book(bookTitle.value);
+    myLibrary.push(newBook);
+    createBook();
+}
+
+function createBook () {
+    let newDiv;
     for (let i = 0; i < myLibrary.length; i++){
         // creates divs based off books in array
-        let newDiv = document.createElement("div");
-        newDiv.className = "book";
-        newDiv.id = "book" + (i+1)
-        libDisplay.appendChild(newDiv);
+                newDiv = document.createElement("div");
+                newDiv.className = "book";
+                newDiv.id = "book" + (i+1)
+                newDiv.innerHTML = myLibrary[i].title;                
     }
+    libDisplay.appendChild(newDiv); 
+
     console.log(libDisplay);
+
+
 }
 
 
-addBookToLibrary();
+
 
 
 // form toggle controls
@@ -35,6 +57,12 @@ formButton.addEventListener("click", function() {
 closeButton.addEventListener("click", function () {
     toggleForm();
 })
+
+submitButton.addEventListener("click", function() {
+    addBookToLibrary();
+})
+
+
 
 
 // things to do
